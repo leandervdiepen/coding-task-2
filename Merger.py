dbb46ad144308c587bb80a53b415ca00cd1cf99b
Leander van Diepen <60552823+lelocodelo@users.noreply.github.com>
2021-02-22 21:23:43 +0100

    Update with input + output, Makefile added, Readme

--- Merger.py
+++ Merger.py
@@ -1,29 +1,61 @@
 from operator import itemgetter
+import ast
+"""
+ #  This program has the function MERGE, which takes a list of lists that represent intervals and merges all overlaping intervals
+ #  into one and returning all merged intervals into an output file named output.txt.
+ #  @author Leander van Diepen
+ #  @license MIT
+ #  @version 1.0
+ #  @date 2/22/2021
+"""
 
-"""
- #  Gets intervals as input and merges all overlapping intervals into one and returns the new list of intervals.
- #  @param intervals The input list of intervals as a two-dimensional integer array.
- #  @return a two dimensional array consisting of the merged intervals
-"""
 
 def MERGE(intervals):
-## Sort the list by the value of the start of the interval
-    intervals.sort(key=itemgetter(0))
-## Create a list which holds the merged intervals
-    toUpdate = []
-## Append the first interval to the list
-    toUpdate.append(intervals[0])
+    if len(intervals) < 1:
+        print("Der übergebene Parameter intervals ist leer oder falsch formatiert. Stelle sicher, dass Daten in input.txt bereitgestellt und richtig formatiert ist.")
+        return
+# Sort the list by the value of the start of the interval
+    intervals.sort(key=itemgetter(0))  # O(1)
+# Create a list which holds the merged intervals
+    nowMerged = []  # O(1)
+# Append the first interval to the list
+    nowMerged.append(intervals[0])  # O(1)
 
-## Loop through all intervals
-    for i in range(1,len(intervals)):
-    ## Check if the last interval in the list of toUpdate (the interval with which merges are still possible) is overlapping with the interval currently inspecting
-        if toUpdate[-1][0] <= intervals[i][1] and toUpdate[-1][1] >= intervals[i][0]:
-        ## It is overlapping. Now check if upper bound of the new overlapping interval is higher, if so update the upper bound.
-            if toUpdate[-1][1] < intervals[i][1]:
-              toUpdate[-1][1] = intervals[i][1]
-        else:
-        ## It is not overlapping. All posible merges with current interval are done. Append the new interval to the list, now
-        ## this interval will get merged accordingly.
-          toUpdate.append(intervals[i])
-## Return the List of 
-    return toUpdate
+# Loop through all intervals
+    for i in range(1, len(intervals)):  # O(n)
+        # Check if the last interval in the list of nowMerged (the interval with which merges are still possible) is overlapping
+        # with the interval currently inspecting
+        # O(1)
+        if nowMerged[-1][0] <= intervals[i][1] and nowMerged[-1][1] >= intervals[i][0]:
+            # It is overlapping. Now check if upper bound of the new overlapping interval is higher, if so update the upper bound.
+            if nowMerged[-1][1] < intervals[i][1]:  # O(1)
+                nowMerged[-1][1] = intervals[i][1]  # O(1)
+        else:  # O(1)
+            # It is not overlapping. All posible merges with current interval are done. Append the new interval to the list, now
+            # this interval will get merged accordingly.
+            nowMerged.append(intervals[i])  # O(1)
+# Return the List of merged intervals
+    return nowMerged
+
+
+def __main__():
+    intervals = []
+# Read intervals from input.txt with ';' as delimiter between intervals in format '[x,y]'
+    inputs = open("input.txt", "r")
+    for line in inputs:  # O(1)
+        intervalsString = line.split(";")
+        for interval in intervalsString: # O(n)
+            intervals.append(ast.literal_eval(interval))
+    inputs.close()
+
+# Run function MERGE and do not declare output as variable to save space
+
+# Write output to a file output.txt in the same format as the input.txt
+    f = open('output.txt', 'w')
+    for item in MERGE(intervals):  # O(n)
+        print(item)
+        f.write(str(item)+';')
+    f.close()
+
+
+__main__()
---
+++ Readme.md
@@ -0,0 +1,25 @@
+Dieses Programm verfügt über die Funktion MERGE, die eine Liste von Listen entgegennimmt, die Intervalle enthalten,
+ und alle sich überschneidenden Intervalle zu einem zusammenführt und alle zusammengeführten Intervalle in eine Ausgabedatei namens output.txt zurückgibt.
+
+## How to Use ##
+Geben Sie eine Liste der Intervalle `[x,y]`, die Sie zusammenführen möchten, in einer Datei "input.txt" im selben Verzeichnis an und verwenden Sie ';' als Trennzeichen.
+
+Starten Sie das Programm mit dem Befehl `make run`
+
+## Laufzeit ##
+Die Laufzeit für das Programm ist in O(n) es werden hauptsächlich einfache for-schleifen verwendet. 
+In den Zeilen 45 & 47 haben wir eine geschachtelte for-schleife. Zeile 45 läuft über alle Zeilen der Input Datei (im einfachen Fall nur eine Zeile mit n Elementen). 
+Das sorting geschieht über effeziente built in Algorithmen.
+Auch bei großen Eingaben, läuft das Programm effizient und nahezu in linearer Zeit.
+
+## Robustheit ##
+Das Programm ist auf die richtige Formattierung der Intervalle in der input.txt-Datei angewiesen, sowie der richtigen Namensgebung dieser Datei.
+Ebenso liest das Programm lediglich aus einem Dateityp Daten aus. befinden sich die Intervalle in einer Datei in anderem Format muss, das Programm angepasst werden.
+
+## Speicher ##
+Der Speicherverbrauch ist angemessen auch für große Dateneingaben. Es werden lediglich zwei "große" variablen angelegt, welche sind `intervals` und `nowMerged`, wobei `nowMerged` meist sehr viel kleiner als `intervals` ist, da diese alle merged Intervalle enthält und `intervals` alle regulären Intervalle als integer Liste.
+
+@author Leander van Diepen
+@license MIT
+@version 1.0
+@date 2/22/2021
